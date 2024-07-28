6906d30326902a49fa7bcf9e7de05cdfe07e2ad6
Jose <josecano0422@gmail.com>
2024-07-27 21:01:24 -0400

    Static web interface for Todo app

--- todo_functions.py
+++ todo_functions.py
@@ -1,12 +1,10 @@
 file_path = "todos.txt"
 
 def get_todos(file_path=file_path):
-
     with open(file_path, 'r') as file:
         todos = file.readlines()
-        return todos
+    return todos
 
 def write_todos(todos_arg, file_path=file_path):
-
-    with open(file_path, 'w') as file:
-        file.writelines(todos_arg)
+    with open(file_path, 'a') as file:
+        file.write(todos_arg)
--- web.py
+++ web.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import todo_functions
 
+todos = todo_functions.get_todos()
 
+def add_todo():
+    todo = st.session_state['new_todo'].strip()
+    if len(todo) > 0:
+        todo_functions.write_todos(todo + "\n")
+
+    st.session_state['new_todo'] = ''
 
 st.title("Todo App")
 
-todos = todo_functions.get_todos()
 
-for todo in todos:
-    st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new to do...")
+for idx, todo in enumerate(todos):
+    if len(todo.strip()) > 0:
+        st.checkbox(todo, key=idx)
+
+st.text_input(label=" ", placeholder="Add a new to do...",
+              on_change=add_todo, key='new_todo')
+
